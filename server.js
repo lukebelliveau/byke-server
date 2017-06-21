a979fbdc7c4335c4210071c6224682f67ecc0b58
lukebelliveau <lbellive@thoughtworks.com>
2017-06-21 14:45:11 -0500

    replaced apollo graphQL with reference implementation

--- server.js
+++ server.js
@@ -1,58 +1,37 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+const { graphql, buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql');
 var fetch = require('node-fetch');
-var { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
-var { makeExecutableSchema } = require('graphql-tools');
-var GraphQLJSON = require('graphql-type-json');
-
-let rawDivvyData = '';
-
-var typeDefs = [`
-
-scalar JSON
-
-type Query {
-  allStations: [Station]
-}
-
-type Station {
-  latitude: Float
-  longitude: Float
-}
-
-schema {
-  query: Query
-}`];
-
-var resolvers = {
-  Query: {
-    allStations() {
-      const rawStations = getAllDivvyStations();
-      const stations = rawStations.map(station => ({ latitude: station.latitude, longitude: station.longitude }))
-      return stations
-    }
-  },
-
-  JSON: GraphQLJSON
-};
-
-var schema = makeExecutableSchema({typeDefs, resolvers});
-var app = express();
-app.set('port', (process.env.PORT || 4000));
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
-app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
 
 const url = 'https://feeds.divvybikes.com/stations/stations.json';
-const getRawDivvyData = () =>
-  fetch(url)
-  .then(response => response.json()
-  .then(json => rawDivvyData = json))
 
-const getDivvyStationByIndex = (index) => rawDivvyData['stationBeanList'][index];
+const schema = buildSchema(`
+  type Query {
+    hello: String
+    allStations: [Station]
+  }
+  
+  type Station {
+    latitude: Float,
+    longitude: Float,
+  }
+`)
 
-const getAllDivvyStations = () => rawDivvyData['stationBeanList']
+const root = {
+  allStations: () => {
+    return fetch(url)
+      .then(response => response.json()
+        .then(json => json['stationBeanList']))
+  }
+}
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Now browse to localhost:${app.get('port')}/graphiql`);
-  getRawDivvyData();
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log('Started graphQL server!');
 });
